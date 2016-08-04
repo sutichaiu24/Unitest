299c272f9ef6d850f80b2b47d4aaaf1fd7c3186f
sudhichai ungsuthornrungsi <oah.s@excelbangkok.com>
2016-08-04 23:44:42 +0700

    ICal update

---
+++ ClassLibrary1/ClassLibrary1/InvesmentCaculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class IVCaculator
+    {
+        public struct Result
+        {
+            public decimal FinalValue;
+            public decimal CompoundInterestRate;
+            public decimal TotalContributions;
+            public decimal InterestEarnedOverTime;
+            
+        }
+        public Result InvestmentCalculator(decimal startBalance,decimal interesRatePerYear,decimal monthlyContribution ,int contributionPeriod)
+        {
+            Result result;
+            result.FinalValue = 5;
+            result.CompoundInterestRate = 10;
+            result.TotalContributions = 10;
+            result.InterestEarnedOverTime = 10;
+
+            return result; 
+        }
+    }
+}
---
+++ ClassLibrary1/ClassLibrary1/InvestmentCalculator_UnitTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+
+namespace ClassLibrary1
+{
+    [TestFixture]
+    public class Class1
+    {
+        private IVCaculator cal;
+
+        [SetUp]
+        public void Init ()
+        {
+            cal = new IVCaculator();
+        }
+        [TestCase(100,5,100,10,500)]
+        public void InvestmentCalculator_WithValidInvestmentParamter_ShouldreturnValidFinalResult(decimal startBalance,decimal interestRatePerYear ,decimal monthlyContribution ,int contributionPeriod )
+        {
+            //Arrange
+
+            //Act
+            var iResult = cal.InvestmentCalculator(startBalance, interestRatePerYear, monthlyContribution, contributionPeriod);
+
+            //Assert
+            Assert.AreEqual(100, iResult.FinalValue);
+
+          
+        }
+
+    }
+}
